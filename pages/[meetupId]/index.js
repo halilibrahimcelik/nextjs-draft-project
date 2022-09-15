a58c1d720669cdae159985eebf11cf4fc0a7dc1d
halilibrahimcelik <halil.celik139@gmail.com>
2022-09-15 10:44:31 +0300

    Added first pages to the project

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>dinamik sub folder</div>;
+};
+
+export default index;
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const index = () => {
+  return <div>new meetup</div>;
+};
+
+export default index;
